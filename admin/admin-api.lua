6205fa9f25f71772cb3cd1c4c3457205c10e6473
easonlin <easonlin@catchplay.com>
2017-03-28 17:30:50 +0800

    Add getting partner api info feature

--- admin/admin-api.lua
+++ admin/admin-api.lua
@@ -4,12 +4,20 @@ local method = ngx.var.request:split(" ")[1]
 ngx.log(ngx.DEBUG, method)
 ngx.log(ngx.DEBUG, path)
 
-
-
 ngx.log(ngx.DEBUG, "Partner: " .. ngx.var.partner)
 ngx.log(ngx.DEBUG, "ApiName: " .. ngx.var.apiName)
-ngx.say("<p> .GET /apis/{partner name}/{api name}  - get specfic partner API info </p>")
-ngx.say("<p> POST /apis/{partner name}/{api name}  - add partner API </p>")
+
+if method == "GET" then
+  ngx.say("<p> .GET /apis/{partner name}/{api name}  - get specfic partner API info </p>")
+elseif method == "POST" then
+  ngx.say("<p> POST /apis/{partner name}/{api name}  - add partner API </p>")
+end
+
+
+
+
+
+
 
 
 
--- admin/admin-partner.lua
+++ admin/admin-partner.lua
@@ -3,8 +3,33 @@ local path = ngx.var.request:split(" ")[2]
 ngx.log(ngx.DEBUG, path)
   
 ngx.log(ngx.DEBUG, "Partner: " .. ngx.var.partner)
-ngx.say("<p>.GET /apis/{partner mame} - get specfic partner all customize API </p>")
+
+ngx.header.content_type = "application/json"
+
+local plugins = "data/partner-api.json"
+
+local f = io.open(plugins, "rb")
+local content = f:read("*all")
+f:close()
+
+local json = cjson.new().decode(content)
 
 
+local partnerTab
 
+for _,partner in pairs( json.partner ) do
+  if partner["partner-name"] == ngx.var.partner then
+    partnerTab = partner
+    break
+  end
+end
 
+if partnerTab == nil then
+  local json = cjson.encode({
+    message = "can not find " .. ngx.var.partner
+  })
+ngx.say(json)
+return
+else
+  ngx.say( cjson.encode(partnerTab))
+end
